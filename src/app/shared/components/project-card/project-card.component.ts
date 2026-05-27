b35b034ce5265cdd9f462cd379c578540088ea9a
douglasscdoug <douglasscdoug@gmail.com>
2026-05-27 17:40:22 -0300

    chore: configuração inicial da estrutura do portfólio

---
+++ src/app/shared/components/project-card/project-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  imports: [],
+  templateUrl: './project-card.component.html',
+  styleUrl: './project-card.component.scss',
+})
+export class ProjectCardComponent {}
