a7337acda5110f915b65ff4f6a2ce9c6352ae1cf
douglasscdoug <douglasscdoug@gmail.com>
2026-05-28 20:50:03 -0300

    feat: melhorar a identidade visual da navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CollapseModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  public isCollapsed = true;
+
+  public toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
