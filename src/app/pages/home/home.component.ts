079f94eed4fcc57b1fc574bf27f0546a084fd71a
douglasscdoug <douglasscdoug@gmail.com>
2026-05-29 16:07:37 -0300

    feat: add componente footer e define layout base da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from "../../shared/components/hero/hero.component";
+import { AboutComponent } from "../about/about.component";
+import { SkillsComponent } from "../../shared/components/skills/skills.component";
+import { ProjectsComponent } from "../projects/projects.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
