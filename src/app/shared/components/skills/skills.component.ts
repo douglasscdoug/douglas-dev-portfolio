f216ee06af945827c5ee65cafd43b8cf6b14d1d5
douglasscdoug <douglasscdoug@gmail.com>
2026-06-02 19:00:11 -0300

    feat: add skills section

--- src/app/shared/components/skills/skills.component.ts
+++ src/app/shared/components/skills/skills.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Skill } from '../../models/skill';
+import AOS from 'aos'
 
 @Component({
   selector: 'app-skills',
@@ -6,4 +8,74 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {}
+export class SkillsComponent implements OnInit {
+  public skills: Skill[] = [
+    {
+      name: 'Angular',
+      icon: 'devicon-angularjs-plain colored',
+      category: 'Front-end'
+    },
+    {
+      name: 'TypeScript',
+      icon: 'devicon-typescript-plain colored',
+      category: 'Front-end'
+    }, {
+      name: 'HTML5',
+      icon: 'devicon-html5-plain colored',
+      category: 'Front-end'
+    }, {
+      name: 'CSS3',
+      icon: 'devicon-css3-plain colored',
+      category: 'Front-end'
+    }, {
+      name: 'Bootstrap',
+      icon: 'devicon-bootstrap-plain colored',
+      category: 'Front-end'
+    }, {
+      name: 'C#',
+      icon: 'devicon-csharp-plain colored',
+      category: 'Back-end'
+    }, {
+      name: '.NET',
+      icon: 'devicon-dot-net-plain-wordmark colored',
+      category: 'Back-end'
+    }, {
+      name: 'ASP.NET Core',
+      icon: 'devicon-dotnetcore-plain colored',
+      category: 'Back-end'
+    }, {
+      name: 'Entity Framework Core',
+      icon: 'devicon-entityframeworkcore-plain colored',
+      category: 'Back-end'
+    }, {
+      name: 'REST APIs',
+      icon: 'devicon-swagger-plain colored',
+      category: 'Back-end'
+    }, {
+      name: 'JWT Authentication',
+      icon: 'devicon-oauth-plain colored',
+      category: 'Back-end'
+    }, {
+      name: 'SQL Server',
+      icon: 'devicon-microsoftsqlserver-plain-wordmark colored',
+      category: 'Banco de dados'
+    }, {
+      name: 'Git',
+      icon: 'devicon-git-plain colored',
+      category: 'Ferramentas'
+    }, {
+      name: 'GitHub',
+      icon: 'devicon-github-original',
+      category: 'Ferramentas'
+    },
+    {
+      name: 'VS Code',
+      icon: 'devicon-vscode-plain colored',
+      category: 'Ferramentas'
+    }
+  ];
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+}
