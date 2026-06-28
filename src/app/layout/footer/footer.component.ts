87081231505af063df15b5964352f3340c2f0606
Douglas Cabral <77368764+douglasscdoug@users.noreply.github.com>
2026-06-28 16:47:58 -0300

    feat: melhoria no botão de voltar ao topo com visibilidade ao rolar a página. (#2)

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public showBackToTop = false;
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void{
+    this.showBackToTop = window.scrollY > 300;
+  }
+
+  public scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+}
